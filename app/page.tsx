a3a4001b28a3f8dff6dd4d9cc1bf3f534cba6fa3
Muhammad Amoo <druxambmarketing@gmail.com>
2024-07-10 02:38:25 -0700

    @druxamb: fixed responsiveness for md screens

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-3 md:px-28">
+    <main className="p-3 lg:px-28">
       <NavBar />
       <HeroSection />
       <Footer />
